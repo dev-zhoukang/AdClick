aec37038e5f4dfb9e9a881ec948ae2bb7ed30758
周康 <dev.zhoukang@gmail.com>
2019-06-01 14:22:24 +0800

    Add: AdClick.php

---
+++ AdClick.php
@@ -0,0 +1,296 @@
+<?php
+
+class AdClick {
+    public static $DISTRIBUTORS = [
+/* chunling */
+	"32282283" => "16AE-4623-BD8A-2C46CCC890DB",
+        "F9CA1C96" => "BE12-47CA-86E0-B80E23BFAFC8",
+        "02CF6D6D" => "29B9-4B30-A589-02541219983E",
+        "66D1DB34" => "FF07-4B6A-9C2A-AF4674512480",
+        "1BA84BDF" => "86FC-4859-80E7-26D78A815557",
+        "B2B4A179" => "E20E-4BE9-9F6E-678EEE0DCFD2",
+        "987DEA96" => "04C4-4F77-BE54-D119FBDB67B8",
+        "AC00AD8E" => "BD0B-4E3B-ADAB-899CE1F715D9",
+        "296346A9" => "B509-421E-AB26-4C2677604F29",
+        "D0E3993D" => "0833-4601-ABFE-ED39F392E6C1",
+        "FDFE482F" => "3E01-486E-B026-D375DDD9173B",
+        "D7AFBC40" => "C933-45CC-9D7B-25DCE3F21DA1",
+        "5FE080AF" => "F557-4841-A55C-6A1C85F84E14",
+        "72AF8AFA" => "8BE9-4B33-A548-645DE66AF9B6",
+        "95AEE26A" => "92DF-4D5A-9E0D-B3CF1FA50C38",
+        "F6ED39DA" => "81B3-4E90-98E0-30AECCF8B3D4",
+        "9C09A7FF" => "3EDA-4D93-8CB2-2A19F67C7A11",
+        "3A7300B7" => "4E30-4F80-A836-7C056EED6345",
+        "9B018834" => "876D-4EEC-BC21-1FCAB443A156",
+        "F01EB555" => "46E8-4909-B9E4-A4D04747CD67",
+        "18F8EA10" => "E9D6-4BFA-AC28-990022271F38",
+        "C786472A" => "B79E-455E-AE04-29FD5B945438",
+        "1576FD96" => "E69E-4671-B3E7-99B045D9EBC3",
+        "7E3312FE" => "1696-4E2C-A483-2DB51CAB2DF7",
+        "73F0BE81" => "5137-45D0-BC4D-E73DD0CB2B00",
+        "538C96B3" => "44FD-4E5E-8656-2F658CB9D889",
+        "4D6D8B9C" => "55E2-45A3-9006-903518B5975D",
+        "878D8081" => "A1DF-4111-BAFF-971E18324FD6",
+        "A6DA1BF3" => "C95B-40C6-8012-7F0229928B8E",
+        "956834B3" => "D6E9-4B65-931B-945523D034A1",
+        "2B68E7B4" => "2FF7-4832-AF03-53D1EDFAF07C",
+        "9C2C4CFE" => "3F17-43DC-AD54-07CFB209A0E7",
+        "661D4FE3" => "1D95-4E82-8F59-67AE655DBD9E",
+        "19BC9A2B" => "9D59-4D8A-9FBE-E8367DF633D5",
+        "2039C3B2" => "FE1B-40D2-879D-959ACA8EDEAA",
+        "B947E2AC" => "253F-48C4-AC7D-50DCDC4516FB",
+        "CA625B81" => "805A-472C-BF11-E082C5FF3AA1",
+        "4CBB016F" => "EC55-4F4C-8A3A-BEC8AEDCF3FB",
+        "A9785D63" => "F611-4902-B419-2D3EFE069B1E",
+        "0ECFF488" => "DF57-4046-870F-3ABB1B5F04EC",
+        "9196B4D4" => "D8CF-4E9B-9A14-DF2586A56825",
+        "7B0F94D9" => "EB04-440C-9F3B-6CB4EEBD25BA",
+        "29A8A964" => "1094-480A-83E1-152C1F030DF0",
+        "8E866346" => "6C70-46E1-9C70-5B13E20380D8",
+        "240662CB" => "0E44-45B6-8308-0D934B0880BD",
+        "165D4E80" => "DE4B-4E6A-8D57-78A260FE88DF",
+        "3B239CB8" => "A034-4AAE-A573-A8DF05252B08",
+        "F63C8B5A" => "6757-422A-841F-A23468AF776C",
+        "D133B199" => "C561-417F-BED0-D7ED2AA0C168",
+        "A4243F9B" => "599B-4911-AFE3-7A56A9EE3478",
+        "2F5726E2" => "CFD8-4D2A-85CB-94F5DA6B2579",
+        "E53B9AEE" => "1B1B-4630-883E-C0A1599576C7",
+        "D3D95DE9" => "62B5-447A-8E1D-291216CE9199",
+        "803648DA" => "1C9A-4B5F-8F4C-27FD1A869D18",
+        "9C3F2134" => "44C2-4FAA-BBA1-E8B37D24E8A7",
+        "5E3FAB21" => "551B-4689-8B81-46D20F733E9A",
+        "A16FC937" => "2C9B-4693-98C5-08C2216F3FC1",
+        "43A94B67" => "AFAB-473E-B902-85CBE663959E",
+        "2F659E90" => "A3DC-40E0-9543-ED9AE9E92040",
+        "64887395" => "898C-465A-BBF1-9C210F5A27C7",
+        "5CA2FC1D" => "D4C2-433F-A473-C711484E2439",
+        "8DAF23E6" => "D5A6-4E41-A023-AD4C41047284",
+        "1BCBFF2A" => "5B95-4BBF-98B1-A112F0C0CE89",
+        "20192518" => "E3B4-4481-B9F1-7ECB88DA4C8C",
+        "75544B95" => "077A-4F5D-B930-E292E18C6D3E",
+        "913E942F" => "FD95-408F-9038-CF6B915D76CA",
+        "8D1D51D6" => "6EB6-412F-A64E-4D07928F3176",
+        "56B1D756" => "3117-4098-8F1D-D391BDA84571",
+        "2003D9EE" => "7354-44EF-80E0-DCF5971398EB",
+        "65B3AF01" => "322B-45E9-856A-5E3307C5BF1A",
+        "8BABB7C5" => "E8C8-4564-A90E-92101C1D095D",
+        "C5F48289" => "4E0E-4C1B-BDBD-6081F02FD79C",
+        "45424318" => "AA46-403A-A1BA-09FC8E58BF73",
+        "15CDB347" => "612D-4C8D-9A9D-1D81E5812527",
+        "BDEEC4CC" => "8C97-4F80-BA32-234AD0316E66",
+        "92E9387F" => "DFC9-46C4-9FE8-CD7CC7B71E10",
+        "5B71E613" => "78B8-42B4-A130-FC71FB302EF9",
+        "C4E9DD8A" => "B92E-416C-972A-ADDD6158B7C7",
+        "EA6A0C91" => "7C07-42F9-AD11-E4BDF1D40EEC",
+        "2F29D5E4" => "B54B-4135-9832-8669F22C3BAD",
+        "587C78A8" => "60CC-4898-8158-B13ED23908B3",
+        "B04ADA87" => "CA6A-4396-86D4-505BC410502A",
+        "DFFE19F4" => "72BF-480D-9485-CA902592FA44",
+        "CCA5E999" => "B851-483F-B6BB-2E50F62EE37F",
+        "8C409488" => "F5EB-42F2-8E89-8A42561585B7",
+        "D128F667" => "3407-471F-A67B-F12294BCC57E",
+        "E19A2173" => "7A36-41F6-9CB3-FD589B97717B",
+        "4138FF65" => "4F0E-48E0-8018-E779335E1A70",
+        "8A32B44A" => "0118-49C7-8CA8-5565AFD7A83A",
+        "207AC509" => "6073-4A2A-AA9B-AF3C633BD0CA",
+        "D5F8D3F3" => "38AA-40FB-B17F-0D2B23CF102F",
+        "B8078A3C" => "C92F-4451-A94D-34D34CDD1BB3",
+        "C07C5CCA" => "5A45-4DB6-92C3-F04EBD291D65",
+        "5124D5FF" => "BCF3-4214-9BE4-8BB54BF58F21",
+        "FBC4A902" => "A475-415A-8DAB-FAA5B2D95716",
+        "FB6FF176" => "B5B5-499F-A3B6-80D87077FD7D",
+        "FF70E8B2" => "2A23-4E4C-9F46-2308D7FD2DDD",
+        "6D602BDE" => "E51F-48C6-83F3-FDEBE3569B08",
+        "ADB234F5" => "0B93-4FD5-826D-9D8FF71D3CDE",
+        "F2905D71" => "0580-4C7D-B270-5447F50E4EF0",
+/* xuewei */
+        "33B1E4CE" => "7C78-4310-B3C4-530559708958",
+        "35A4D507" => "3711-43A0-A7D0-CF7E06D6FF7E",
+        "03FC2B5D" => "3CFC-441D-B80E-E818072FAEF8",
+        "8E0F348A" => "563C-4C84-A6C2-DA050B5D0313",
+        "5BD908F8" => "E9CD-47F5-9B73-AFADC331591E",
+        "2709A5B3" => "D87E-4D4E-9A03-4477A9FFDB0D",
+        "29CDDD6E" => "4887-4212-BE5B-19BB90C41DBD",
+        "F0746FE5" => "FB4C-4061-9F0B-C795B0AAF54D",
+        "DB010CC1" => "E2CF-48D7-A29F-0DD1F31AC5A6",
+        "1090FE9A" => "1375-47CB-93B8-B1434B3F81C4",
+        "5FE5F192" => "6097-4F8B-9FC0-076664A6CC28",
+        "9A223BBA" => "5EFF-4BA5-9A50-2237D35CAC24",
+        "A7F6307E" => "50A9-4CCC-B77B-124B6505E887",
+        "02D920F3" => "2FFA-463B-A67A-374DEDCC5C2A",
+        "97EE66C7" => "3261-4DC7-9EF2-08B73CC6F51A",
+        "3CD673D3" => "3E90-4B9A-A587-8BD48429D774",
+        "7F0C6998" => "0DD2-420E-B885-6E8A88586D5F",
+        "66C8EBA7" => "2606-46F0-8FF2-F48F79115763",
+        "09CAB47E" => "E963-4B7F-BE7F-57207E1C6357",
+        "658FC54E" => "ADF8-416D-BFF1-F94C6FD07293",
+        "20D0A99C" => "38DD-4213-9685-ED5140197202",
+        "CFD14A7C" => "8A36-4B7A-8A0A-FC486E3C4672",
+        "0F296D50" => "B32F-464A-B2DE-D457FA8FC93A",
+        "1C99E5E9" => "4FCC-4AAF-9020-1931A754393D",
+        "9CD63045" => "DFE3-4390-8031-C10D996D280D",
+        "0D3EA333" => "A109-4C5B-861E-75C8224B835E",
+        "BC1A82DF" => "8730-448E-9BE0-F8A625732AB6",
+        "4FF2502E" => "628E-4957-92A9-19C8D6D4FC3E",
+        "CFA4CAF6" => "AB51-400E-85BC-1002A17AAC3E",
+        "100F6F3B" => "41C4-44B1-97F9-4F117FCFE962",
+        "EE0AD4C8" => "9DA4-4714-BBB7-317AD902CE05",
+        "62FCD984" => "5A64-4883-9A7F-ABCEAC155898",
+        "73A56F7C" => "3A4A-4EDA-B24D-E267E7FB2412",
+        "8788D408" => "A3E5-4ED2-896E-DE02D85C83D6",
+        "7C98034E" => "768D-4764-B845-89C787EA5A59",
+        "A4E8565C" => "A8E2-4D5C-90AA-1CCA8E076156",
+        "FD4AD87F" => "A23D-4B1F-B368-0FB4E08CFAB3",
+        "B670A396" => "ACBD-4145-A943-0E6B57A75A77",
+        "42934FC1" => "7667-427F-9B42-A23B51C275E1",
+        "93ABB407" => "CD38-4852-8577-41566424CFE2",
+        "0B819F89" => "E89F-42EA-B80B-4FB910184EE7",
+        "22532F98" => "D6C5-4D79-BE4F-8D92836043F1",
+        "C9D9A41C" => "EFF6-4AF3-8FE0-6B875D690B68",
+        "A381C71D" => "2E0A-4576-9588-746FBC66DF3B",
+        "278E9107" => "9CC6-4CD5-9B71-F15C4CF158AD",
+        "5B63952F" => "FEAF-4963-BB45-05A3B5664942",
+        "2CFEA657" => "2C35-4E15-BEA5-1FFFBAA4BEFD",
+        "8E0A434E" => "5069-4ABD-BCE4-1D954EF0927F",
+        "44419D4F" => "0643-44EF-ACCC-59EBBD411D41",
+        "8F5E3858" => "C0B4-47BE-B7FF-0AB9F8796139",
+        "9B7E6C17" => "3CD7-4150-BCA4-7280BB081892",
+        "AFF016EE" => "565B-45CE-89FC-4A65993FFC37",
+        "421FC7F6" => "A3A2-4D38-94F0-5C9AEB8ECFFE",
+        "E496695B" => "9F64-416F-A06B-3DDE43649D51",
+        "E4034186" => "E0C9-427D-8EDD-5B04DA23D382",
+        "898767DE" => "CC2D-4E27-B3F8-C0EF2F8C2046",
+        "85B697DF" => "E0F7-4DFE-901C-14D4D014F080",
+        "5DAFF3ED" => "5F1B-4337-B1EE-6D06D19B39DB",
+        "49FAC56A" => "52A9-4E91-B644-2143C912653A",
+        "0B8C3C34" => "025C-4174-9B4F-A96CD051EFEE",
+        "433794FB" => "57ED-450B-AA02-017F5E3B6948",
+        "9B8B0D9F" => "2BCB-4C14-BCF0-9E042FAF435A",
+        "EC4C4DB3" => "AAC2-42DB-922A-FC9952BF1D57",
+        "B00FF39F" => "E7BE-47A1-A9F0-3D7BA16F009F",
+        "54ADA145" => "554A-4927-A794-FBA469338A56",
+        "558AD714" => "13AB-41D8-AD65-1A5925A86A47",
+        "1A8B095B" => "0338-4A51-8F69-48BD20484C7E",
+        "C8DE65B0" => "AD74-4414-9B9E-BAC368FADF0B",
+        "B9365852" => "B793-44AC-8460-FF293B446D9C",
+        "6C9AD77C" => "B72F-4D33-836A-1497CC36F483",
+        "682868B9" => "B618-430B-A73C-0CF48164B89A",
+        "85C6B8CC" => "8D13-4300-A52A-626106A36684",
+        "A9CDAFE1" => "6CFC-4AE6-96FD-EBF65086B1C6",
+        "55EF0360" => "D244-4ED1-8A70-E1E757D35764",
+        "3D3B2494" => "9BB4-41C3-80C2-C97BBC663D4F",
+        "CFCB74CD" => "C46C-46DF-98C0-DA0CC5E7054E",
+        "22FF5B4E" => "6BE0-4991-A3A8-CEB4D8B1090B",
+        "F76BA1DD" => "86D8-4DB4-9EE4-4BFA3AE78058",
+        "FC826861" => "0A1D-4839-B01C-A8527D320BDC",
+        "F846D696" => "6E73-49C9-8C3F-5F312E4F3DB6",
+        "566FF5D4" => "E04F-4D0E-826D-BDA5393E6834",
+        "3276D4DF" => "F647-41B5-9827-F041DE5975F8",
+        "5F500EBB" => "E060-4BCA-8523-76B1E3E49AF1",
+        "2622DBF9" => "C6C7-4F8F-90A1-FCD7AC3285A8",
+        "4BA7CB2E" => "EA4C-47AB-A2B3-7242E36E3C49",
+        "B7FB7ABE" => "0559-4009-A2A3-BD6E003CB4C2",
+        "0BEEC6CC" => "9E2E-44DE-B3F1-244A9B13C308",
+        "487BD7EB" => "ED5C-4DA2-9E8B-C5F042637C0F",
+        "12A56B4F" => "E84A-44B7-8882-0EC71303D785",
+        "F712BC1E" => "F471-4404-BD87-26076C421F41",
+        "A869C75D" => "0729-4301-A5C5-72AB37B563D3",
+        "B82931A3" => "DCAF-4F69-98A2-62D8365CF24E",
+        "06BB00FA" => "B6B5-4BE0-A65D-8D27D026E64E",
+        "BCDE7643" => "80F0-49A7-9913-A1B4A9875CD5",
+        "340ED5F7" => "72B5-4F36-87FF-56107243216D",
+        "AC26E1D6" => "3808-4446-9708-4BC60F7D2354",
+        "D9B085B2" => "FC9D-4BB1-93BF-390BADA8E4BE",
+        "6E1AE6A2" => "B151-46DC-A375-F86B6EF8580E",
+        "282479C4" => "13BB-4D8F-9F2B-1746471591CF",
+        "278F0332" => "89A0-4C9C-837A-178EF77A7624",
+        "80C93233" => "3E5A-44A0-BC8F-E3F963D7667D",
+    ];
+
+    private static $TODAY = null;
+
+    private static function today() {
+        if (Predicates::isNull(self::$TODAY)) {
+            self::$TODAY = date('Ymd',time());
+        }
+        return self::$TODAY;
+    }
+
+    private static function key($k) {
+        return $k . "_" . self::today();
+    }
+
+    private static function join($arg1, $arg2) {
+        return $arg1 . "_" . $arg2;
+    }
+
+    private static function address() {
+        $real_ip = Protocol::remoteAddress();
+        return dechex(ip2long($real_ip));
+    }
+
+    public static function click($distributor, $token) {
+        $ip = self::address();
+        $distributor = Protocol::required("d");
+        if (!array_key_exists($distributor, self::$DISTRIBUTORS) || !Predicates::equals(Protocol::required("t"), self::$DISTRIBUTORS[$distributor])) {
+            return;
+        }
+        MocaLog::Write('adclick', 'clk', $ip . "-" . $distributor, 1);
+
+        $redis = Yii::app()->redisnp;
+        $redis->incr(self::key("tc"));
+        $redis->incr(self::key(self::join("dc", $distributor)));
+        $ipKey = self::key(self::join($ip, "c"));
+        $redis->zIncrBy($ipKey, 1, $distributor);
+        $redis->zIncrBy($ipKey, 1, "t");
+        $redis->expire($ipKey, 3600 * 24);
+        return;
+    }
+
+    private static function distributorOf($ip) {
+        $ipKey = self::key(self::join($ip, "c"));
+        $redis = Yii::app()->redisnp;
+        $top = $redis->zRevRange($ipKey, 0, 1);
+        if (Predicates::isEmpty($top) || ($top[0] == "t" && count($top) == 1)) {
+          return null;
+        } else {
+          return $top[0] == "t" ? $top[1] : $top[0];
+        }
+    }
+
+    public static function activate() {
+        $redis = Yii::app()->redisnp;
+        $ip = self::address();
+        $distributor = self::distributorOf($ip);
+        if (Predicates::isNull($distributor)) {
+            return false;
+        }
+        MocaLog::Write('adclick', 'act', $ip . "-" . $distributor, 1);
+        $redis->incr(self::key("ta"));
+        $redis->incr(self::key(self::join("da", $distributor)));
+        
+        $ipKey = self::join($ip, "a");
+        $count = $redis->incr($ipKey);
+        if ($count > 5) {
+            return;
+        }
+        $redis->incr(self::key("tv"));
+        $redis->incr(self::key(self::join("dv", $distributor)));
+        MocaLog::Write('adclick', 'validact', $ip . "-" . $distributor, 1);
+    }
+
+    public static function register() {
+        $ip = self::address();
+        $redis = Yii::app()->redisnp;
+        $distributor = self::distributorOf($ip);
+        if (Predicates::isNull($distributor)) {
+            return '';
+        }
+        MocaLog::Write('adclick', 'reg', $ip . "-" . $distributor, 1);
+        $redis->incr(self::key("tr"));
+        $redis->incr(self::key(self::join("dr", $distributor)));
+        return $distributor;
+    }
+}
+
+?>
